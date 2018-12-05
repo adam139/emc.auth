2ed065ef44177dd112efaeb13029378aabbfbe7c
adam139 <yuejun.tang@gmail.com>
2018-12-05 16:36:51 +0800

    send fake jida request for demo user

--- emc/auth/tests/test_jida_request.py
+++ emc/auth/tests/test_jida_request.py
@@ -10,7 +10,7 @@ def getFile(filename):
 
 class TestRequest(unittest.TestCase):
     def test_post_dummy_jida_requests(self):
-        headers = dict(DNNAME ='CN=顾蕾,T=110101197909181550,O=JIT,C=CN',
+        headers = dict(DNNAME ='CN=李四,T=333010199106113321,O=JIT,C=CN',
                   CLIENTIP = '123.123.123.123'
                   )
         url = 'http://127.0.0.1:8080/Plone8/'
