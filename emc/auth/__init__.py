34a9c65eb1f079d22eb84ace361ab8c716bdb075
adam139 <yuejun.tang@gmail.com>
2020-09-15 21:39:11 +0800

    patch Products.PlonePAS

--- emc/auth/__init__.py
+++ emc/auth/__init__.py
@@ -1,5 +1,20 @@
 # -*- extra stuff goes here -*-
+#-*- coding: UTF-8 -*-
 
+from Products.PlonePAS.plugins.user import UserManager
+from Products.PlonePAS import pas
+from emc.policy.patch.pas import patch_pas
+from emc.policy.patch.user import isRole
+
+
+####################################
+# monkey patch pas, the evil happens
+
+pas.patch_pas = patch_pas
+UserManager.isRole = isRole
+
+
+#################################
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
